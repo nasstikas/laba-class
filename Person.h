5b27aa5aed1eae8b2fff9f0a8b16523944b11439
nasstikas <146032098+nasstikas@users.noreply.github.com>
2024-04-04 17:47:06 +0300

    Update Person.h

--- Person.h
+++ Person.h
@@ -2,6 +2,7 @@
 #define PERSON_H
 #include <iostream>
 using namespace std;
+
 class Person {
 private:
 	string name;
@@ -24,22 +25,22 @@ public:
 	void setName(string& name) {
 		this->name = name;
 	}
-	void setPhone(int& phone) {
+	void setPhone(int phone) {
 		this->phone_number = phone;
 	}
 	void setEmail(string& email) {
 		this->email = email;
 	}
-	void setAge(int& age) {
+	void setAge(int age) {
 		this->age = age;
 	}
 	Person() {
-		name = string("A");
+		name = string("No name");
 		email = string("example@qmail.com");
 		age = 0;
 		phone_number = 0;
 	}
-	Person(string& name, int& phone_number, int& age, string& email) {
+	Person(string& name, int phone_number, int age, string& email) {
 		this->name = name;
 		this->phone_number = phone_number;
 		this->email = email;
