58547b49b70cb09c26e400c3ce4b9b7f6f53dad7
nasstikas <146032098+nasstikas@users.noreply.github.com>
2024-04-04 17:48:54 +0300

    Update Manicure.h

--- Manicure.h
+++ Manicure.h
@@ -1,7 +1,6 @@
 #ifndef MANICURE_H
 #define MANICURE_H
 #include <iostream>
-
 using namespace std;
 
 class Manicure {
@@ -15,6 +14,22 @@ public:
 	}
 	void setType(string& type) {
 		this->type = type;
+		if (type == "French") {
+			price = 1500;
+			ex_time = 180;
+		}
+		else if (type == "Plain color") {
+			price = 1000;
+			ex_time = 90;
+		}
+		else if (type == "Stamping") {
+			price = 1150;
+			ex_time = 120;
+		}
+		else {
+			price = 1300;
+			ex_time = 150;
+		}
 	}
 	int getEx_Time() {
 		return ex_time;
@@ -33,9 +48,20 @@ public:
 		ex_time = 90;
 		price = 1000;
 	}
-	Manicure(string& type, int& ex_time, int& price) {
+	Manicure(string& type, int price) {
 		this->type = type;
-		this->ex_time = ex_time;
+		if (type == "French") {
+			ex_time = 180;
+		}
+		else if (type == "Plain color") {
+			ex_time = 90;
+		}
+		else if (type == "Stamping") {
+			ex_time = 120;
+		}
+		else {
+			ex_time = 150;
+		}
 		this->price = price;
 	}
 };
