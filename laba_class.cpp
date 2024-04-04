37b40a2ae15350d864145d5b8d6cec3eab62963f
Ulyna2 <146021923+Ulyna2@users.noreply.github.com>
2024-04-04 19:32:20 +0300

    Update laba_class.cpp

--- laba_class.cpp
+++ laba_class.cpp
@@ -1,39 +1,64 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Person.h"
 #include "Master.h"
 #include "Worker.h"
 #include "Client.h"
 #include "Manicure.h"
+#include "Staff.h"
 using namespace std;
 int main()
 {
+
 	string name1("Anastasia");
-	string email("no email");
+	string email("@email.ru");
 	string type("French");
-	string a;
-	//cin >> a;
-
-	Master Anastasia(name1, 999, 19, email, 20000, 4, type, 1300, 3);
-	
-	
-	//Anastasia.setName(a);
-	cout << "name: " << Anastasia.getName() << endl;
-	cout << "phone number: " << Anastasia.getPhone() << endl;
-	cout << "age: " << Anastasia.getAge() << endl;
-	cout << "email: " << Anastasia.getEmail() << endl;
-	cout << "salary: " << Anastasia.getSalary() << endl;
-	cout << "tax rate: " << Anastasia.getTaxRate() << endl;
-	cout << "experience: " << Anastasia.getExperience() << endl;
-	cout << "cost: " << Anastasia.getCost() << endl;
-	cout << "type: " << Anastasia.getType() << endl;
-	cout << "execution time: " << Anastasia.getEx_Time() << endl;
-	cout << "price: " << Anastasia.getPrice() << endl;
-	cout << "efficiency: " << Anastasia.getEfficiency() << endl;
-
+	string type1("Plain color");
+	string type2("Stamping");
 	string name2("Ulyana");
 	string day("Friday");
-	Client Ulyana;
-	//Client Ulyana(name2, 555, 18, email, type, 0, 0, day, 17);
-	Ulyana.setType(type);
-	cout << Ulyana.getType() << " " << Ulyana.getPrice() << " " << Ulyana.getEx_Time() << endl;
+	string name3("Maria");
+	string post("Director");
+	string name4("Alexandr");
+	string post1("Security");
+	string name5("Olga");
+	string post2("Administrator");
+	string name6("Vlada");
+	string post3("Сleaning lady");
+
+	cout << "Barankina nails <3" << endl;
+
+	cout << "_________Price list___________" << endl;
+
+	Manicure manicure(type, 1000);
+	cout << manicure << endl;
+
+	Manicure manicure1(type1, 900);
+	cout << manicure1 << endl;
+
+	Manicure manicure2(type2, 1500);
+	cout << manicure2;
+
+	cout << "______________________________" << endl;
+
+	Staff Maria(name3, 666, 22, email, 40000, 10, post);
+	cout << Maria << endl;
+
+	Staff Alexandr(name4, 107, 35, email, 18000, 5, post1);
+	cout << Alexandr << endl;
+
+	Staff Olga(name5, 109, 20, email, 15000, 3, post2);
+	cout << Olga << endl;
+
+	Staff Vlada(name6, 111, 18, email, 10000, 1, post3);
+	cout << Vlada << endl;
+
+	cout << "Master:" << endl;
+	Master Anastasia(name1, 999, 19, email, 20000, 4, type, 1500, 3);
+	cout << Anastasia << endl;
+	
+	cout << "Client:" << endl;
+	Client Ulyana(name2, 555, 18, email, type2, 1500, day, 17);
+	cout << Ulyana << endl;
+
+	
 }
